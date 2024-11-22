bb3f17d07e3ae2f187536f94330548289aa8ed88
Steven Johnson <steveajohn1@gmail.com>
2024-11-22 16:49:15 +0000

    renaming input to memory to be more clear

--- layer6/src/lib.rs
+++ layer6/src/lib.rs
@@ -1,39 +1,39 @@
 mod tomtel;
 
 pub fn execute(input: &[u8]) -> Vec<u8> {
-    let mut input = input.to_vec();
+    let mut memory = input.to_vec();
     let mut vm = tomtel::Tomtel::new();
 
     loop {
         let instruction_pointer = vm.pc() as usize;
-        let opcode = input[instruction_pointer];
+        let opcode = memory[instruction_pointer];
         let result = match opcode {
             0xC2 => vm.add(),
-            0xE1 => vm.advance_pointer(input[instruction_pointer + 1]),
+            0xE1 => vm.advance_pointer(memory[instruction_pointer + 1]),
             0xC1 => vm.compare(),
             0x01 => vm.halt(),
             0x21 => vm.jump_equals_zero(u32::from_le_bytes([
-                input[instruction_pointer + 1],
-                input[instruction_pointer + 2],
-                input[instruction_pointer + 3],
-                input[instruction_pointer + 4],
+                memory[instruction_pointer + 1],
+                memory[instruction_pointer + 2],
+                memory[instruction_pointer + 3],
+                memory[instruction_pointer + 4],
             ])),
             0x22 => vm.jump_not_equals_zero(u32::from_le_bytes([
-                input[instruction_pointer + 1],
-                input[instruction_pointer + 2],
-                input[instruction_pointer + 3],
-                input[instruction_pointer + 4],
+                memory[instruction_pointer + 1],
+                memory[instruction_pointer + 2],
+                memory[instruction_pointer + 3],
+                memory[instruction_pointer + 4],
             ])),
             code if (code & 0b01000000 == 0b01000000) => {
                 let source = opcode & 0b00000111;
                 let destination = (opcode & 0b00111000) >> 3;
 
                 if source == 0 {
-                    let value = input[instruction_pointer + 1];
+                    let value = memory[instruction_pointer + 1];
                     match vm.mvi(destination, value) {
                         // If the destination was ptr+c then write the value to the memory.
                         Some(tomtel::MemoryPointerResult::Write((address, value))) => {
-                            input[address as usize] = value;
+                            memory[address as usize] = value;
                         }
                         _ => (),
                     };
@@ -43,12 +43,12 @@ pub fn execute(input: &[u8]) -> Vec<u8> {
                         Some(tomtel::MemoryPointerResult::Read(address)) => {
                             println!(
                                 "Reading from address: {} -> {}",
-                                address, input[address as usize]
+                                address, memory[address as usize]
                             );
-                            vm.mv(destination, input[address as usize])
+                            vm.mv(destination, memory[address as usize])
                         }
                         Some(tomtel::MemoryPointerResult::Write((address, value))) => {
-                            input[address as usize] = value;
+                            memory[address as usize] = value;
                             None
                         }
                         None => None,
@@ -63,10 +63,10 @@ pub fn execute(input: &[u8]) -> Vec<u8> {
 
                 if source == 0 {
                     let value = u32::from_le_bytes([
-                        input[instruction_pointer + 1],
-                        input[instruction_pointer + 2],
-                        input[instruction_pointer + 3],
-                        input[instruction_pointer + 4],
+                        memory[instruction_pointer + 1],
+                        memory[instruction_pointer + 2],
+                        memory[instruction_pointer + 3],
+                        memory[instruction_pointer + 4],
                     ]);
                     vm.mvi32(destination, value)
                 } else {
